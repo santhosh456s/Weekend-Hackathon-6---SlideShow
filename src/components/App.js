0aaa125b29cd5915e84b435065a24c0059e910d7
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-12-16 09:46:32 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,56 @@
 import React, { Component, useState } from "react";
-import { act } from "react-test-renderer";
 import "../styles/App.css";
-
 const App = (props) => {
   const { slides } = props;
+  //console.log(props);
+  //console.log(slides.length);
 
   const [index, setIndex] = useState(0);
 
-  const title = slides[index].title;
-  const text = slides[index].text;
+  let title = slides[index].title;
+  let text = slides[index].text;
 
-  const Handelclickprev = (action) => {
+  function handleClick(action) {
+    //console.log(action);
     if (action === "prev") {
-      setIndex(index - 1);
-    }
-  };
-  const Handelclicknext = (action) => {
-    if (action === "next") {
-      setIndex(index + 1);
-    }
-  };
-  const Handelclickreset = (action) => {
-    if (action === "reset") {
+      //if (show.index === 0) return;
+
+      let newIndex = index - 1;
+      setIndex(newIndex);
+    } else if (action === "next") {
+      //if (show.index === slides.length - 1) return;
+
+      let newIndex = index + 1;
+      setIndex(newIndex);
+    } else if (action === "restart") {
       setIndex(0);
     }
-  };
+  }
+
   return (
     <>
       <h1 data-testid="title">{title}</h1>
       <p data-testid="text">{text}</p>
       <button
+        onClick={() => handleClick("prev")}
         data-testid="button-prev"
         disabled={index === 0 ? true : false}
-        onClick={Handelclickprev("prev")}
       >
         Prev
       </button>
       <button
+        onClick={() => handleClick("next")}
         data-testid="button-next"
         disabled={index === slides.length - 1 ? true : false}
-        onClick={Handelclicknext("next")}
       >
         Next
       </button>
       <button
+        onClick={() => handleClick("restart")}
         data-testid="button-restart"
         disabled={index === 0 ? true : false}
-        onClick={Handelclickreset("reset")}
       >
-        Reset
+        Restart
       </button>
     </>
   );
